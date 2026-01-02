3829a5e6f688bf1acb8d29bb79acb1305e461860
Oswaldo <rodriosw13@gmail.com>
2026-01-02 11:20:02 -0400

    Adding services

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -7,9 +8,8 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   const hook = () => {
-    axios.get('http://localhost:3001/notes').then((response) => {
-      console.log(response)
-      setNotes(response.data)
+    noteService.getAll().then((inititalNotes) => {
+      setNotes(inititalNotes)
     })
   }
 
@@ -22,8 +22,7 @@ const App = () => {
       important: Math.random() < 0.5,
     }
 
-    axios.post('http://localhost:3001/notes', newNote).then((response) => {
-      console.log(response)
+    noteService.create(newNote).then((newNote) => {
       setNotes([...notes, newNote])
       setNewNotes('')
     })
@@ -37,15 +36,40 @@ const App = () => {
     setShowAll(!showAll)
   }
 
+  const toggleImportance = (id) => {
+    console.log(`importance of ${id} needs to be toggled`)
+    const noteToEdit = notes.find((note) => note.id === id)
+    const edittedNote = { ...noteToEdit, important: !noteToEdit.important }
+
+    noteService
+      .update(id, edittedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
+      })
+      .catch((error) => {
+        alert(
+          `the note ${noteToEdit.content} was already deleted from the server`
+        )
+        setNotes(notes.filter((n) => n.id !== id))
+      })
+  }
+
   const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
   return (
     <>
       <h1>Notes</h1>
-      <button onClick={changeFilter}>{showAll ? 'show important' : 'show all'}</button>
+      <button onClick={changeFilter}>
+        {showAll ? 'show important' : 'show all'}
+      </button>
       <ul>
         {notesToShow.map((note) => (
-          <li key={note.id}>{note.content}</li>
+          <li key={note.id}>
+            {note.content}
+            <button onClick={() => toggleImportance(note.id)}>
+              {note.important ? 'make not important' : 'make important'}
+            </button>
+          </li>
         ))}
       </ul>
       <form onSubmit={addNote}>
