07013689702de41c3d23fa9bb1a3e5e57c1fdb63
Oswaldo <rodriosw13@gmail.com>
2026-01-13 09:58:45 -0400

    Adding error handling with middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,38 @@
 require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
-
 const Note = require('./models/note')
+
 const app = express()
+
+const requestLogger = (req, res, next) => {
+  console.log('Method:', req.method)
+  console.log('Path:', req.path)
+  console.log('Body:', req.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+
+  if (error.message === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(express.json())
-app.use(cors())
 app.use(express.static('dist'))
+app.use(cors())
+app.use(requestLogger)
+
+// --Routes-- //
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
 
 //All notes
 app.get('/api/notes', (req, res) => {
@@ -19,8 +45,8 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
   const body = req.body
 
-  if (body.content === undefined || body.important === undefined) {
-    return res.status(404).json({ error: 'content missing' })
+  if (!body.content || !body.important) {
+    return res.status(400).json({ error: 'content missing' })
   }
 
   const note = new Note({
@@ -34,11 +60,51 @@ app.post('/api/notes', (req, res) => {
 })
 
 //Get a single note
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then((note) => {
-    res.json(note)
-  })
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
+
+//Update a single note
+app.put('/api/notes/:id', (req, res, next) => {
+  const { content, important } = req.body
+  const id = req.params.id
+
+  const newNote = {
+    content,
+    important,
+  }
+
+  Note.findByIdAndUpdate(id, newNote, { new: true })
+    .then((updatedNote) => {
+      res.json(updatedNote)
+    })
+    .catch((error) => next(error))
+})
+
+//Delete a single note
+app.delete('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id
+  Note.findByIdAndDelete(id)
+    .then((result) => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT)
