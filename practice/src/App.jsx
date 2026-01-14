c4264b2d5e8731e290ac965aaaec9ba1598e0a65
Oswaldo <rodriosw13@gmail.com>
2026-01-14 15:10:02 -0400

    Adding linting

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -4,40 +4,6 @@ import noteService from './services/notes'
 import Footer from './Footer.jsx'
 
 const App = () => {
-  //   const [value, setValue] = useState('')
-  //   const [rates, setRates] = useState({})
-  //   const [currency, setCurrency] = useState(null)
-
-  //   useEffect(() => {
-  //     console.log('effect run, currency is now', currency)
-
-  //     if (currency) {
-  //       console.log('fetching exchange rates...')
-  //       axios.get(`https://open.er-api.com/v6/latest/${currency}`).then((response) => {
-  //         setRates(response.data.rates)
-  //       })
-  //     }
-  //   }, [currency])
-
-  //   const handleChange = (event) => {
-  //     setValue(event.target.value)
-  //   }
-
-  //   const onSearch = (event) => {
-  //     event.preventDefault()
-  //     setCurrency(value)
-  //   }
-
-  //   return (
-  //     <div>
-  //       <form onSubmit={onSearch}>
-  //         currency: <input value={value} onChange={handleChange} />
-  //         <button type='submit'>exchange rate</button>
-  //         <pre>{JSON.stringify(rates, null, 2)}</pre>
-  //       </form>
-  //     </div>
-  //   )
-
   const [notes, setNotes] = useState([])
   const [newNotes, setNewNotes] = useState('new note...')
   const [showAll, setShowAll] = useState(true)
